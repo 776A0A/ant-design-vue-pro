86afa88d8c629ddfc6ef8ab14859e6f913f76925
har3dy <har3dy@icloud.com>
2020-01-28 02:16:39 +0800

    使用axios模拟发送请求

--- src/views/Dashboard/Analysis.vue
+++ src/views/Dashboard/Analysis.vue
@@ -5,42 +5,58 @@
 </template>
 <script>
   import Chart from '../../components/Chart';
-  import random from 'lodash/random';
+
+  // import random from 'lodash/random';
+
+  import axios from 'axios';
 
   export default {
     name: 'Analysis',
     components: { Chart },
     data() {
       return {
-        chartOption: {
-          title: {
-            text: 'ECharts 入门示例',
-          },
-          tooltip: {},
-          xAxis: {
-            data: ['衬衫', '羊毛衫', '雪纺衫', '裤子', '高跟鞋', '袜子'],
-          },
-          yAxis: {},
-          series: [
-            {
-              name: '销量',
-              type: 'bar',
-              data: [5, 20, 36, 10, 10, 20],
-            },
-          ],
-        },
+        chartOption: {},
       };
     },
     mounted() {
+      this.getChartData();
+
       // 改变传入的数据，用来测试数据改变后视图的变化
       this.setInterval = setInterval(() => {
-        this.chartOption.series[0].data = this.chartOption.series[0].data.map(
-          () => random(100),
-        );
-        // 通过将变化后的数据解构为一个新的对象即可达到更新整个 chartOption 的目的
-        // 这样可以避免在 watch 中使用深度监听，提高性能
-        this.chartOption = { ...this.chartOption };
+        // this.chartOption.series[0].data = this.chartOption.series[0].data.map(
+        //   () => random(100),
+        // );
+        // // 通过将变化后的数据解构为一个新的对象即可达到更新整个 chartOption 的目的
+        // // 这样可以避免在 watch 中使用深度监听，提高性能
+        // this.chartOption = { ...this.chartOption };
+
+        this.getChartData();
       }, 2000);
+    },
+    methods: {
+      getChartData() {
+        axios
+          .get('/api/dashboard/chat', { parmas: { ID: 12345 } })
+          .then(res => {
+            this.chartOption = {
+              title: {
+                text: 'ECharts 入门示例',
+              },
+              tooltip: {},
+              xAxis: {
+                data: ['衬衫', '羊毛衫', '雪纺衫', '裤子', '高跟鞋', '袜子'],
+              },
+              yAxis: {},
+              series: [
+                {
+                  name: '销量',
+                  type: 'bar',
+                  data: res.data,
+                },
+              ],
+            };
+          });
+      },
     },
     beforeDestroy() {
       clearInterval(this.setInterval);
