0cdcd9f0c8262088f0523098ae91a3f6abf9378f
har3dy <har3dy@icloud.com>
2020-01-28 16:40:47 +0800

    使用函数式jsx个性化错误提醒信息

--- babel.config.js
+++ babel.config.js
@@ -1,8 +1,9 @@
 module.exports = {
-  presets: ['@vue/cli-plugin-babel/preset'],
+  presets: ['@vue/cli-plugin-babel/preset', '@vue/babel-preset-jsx'],
   plugins: [
     [
       'import',
+      // style: true 会加载 less 文件
       { libraryName: 'ant-design-vue', libraryDirectory: 'es', style: true },
     ],
   ],
--- src/utils/request.js
+++ src/utils/request.js
@@ -11,7 +11,14 @@ function request(options) {
       } = err;
 
       notification.error({
-        message: status,
+        // 使用函数式的方式返回VNode，以此来个性化显示错误信息
+        // 语法及配置参考 https://github.com/vuejs/jsx
+        // eslint-disable-next-line no-unused-vars
+        message: h => (
+          <div>
+            请求错误码：<span style="color: red;">{status}</span>：{options.url}
+          </div>
+        ),
         description: statusText,
       });
 
